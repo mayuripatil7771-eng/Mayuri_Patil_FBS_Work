47e938a46ea1c3f3c37c916c2c647025b89d43e0
Mayuri Patil <mayuripatil7771@gmail.com>
2026-07-20 21:48:06 +0530

    Add files via upload

---
+++ swap 2 numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int temp;
+
+    printf("Before Swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After Swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
