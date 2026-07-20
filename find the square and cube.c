a10f2d8c4a90b7876b95aebe9783828dd4e763ab
Mayuri Patil <mayuripatil7771@gmail.com>
2026-07-20 21:45:45 +0530

    Add files via upload

---
+++ find the square and cube.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 5;
+    int square, cube;
+
+    square = num * num;
+    cube = num * num * num;
+
+    printf("Square = %d\n", square);
+    printf("Cube = %d", cube);
+
+    return 0;
+}
